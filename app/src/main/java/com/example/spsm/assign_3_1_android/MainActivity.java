e1bc39042fc08239fe48d2ef6620d294ffa42257
SPSM <saurav_prakash@hotmail.com>
2017-10-09 17:14:46 +0530

    Commiting Again

---
+++ app/src/main/java/com/example/spsm/assign_3_1_android/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.spsm.assign_3_1_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vibgyor);
+    }
+}
